1d39c7e005358b680c0f1e6546a03956a2f1a737
hydfb503s <he000145@algonquinlive.com>
2026-07-13 03:27:21 -0400

    Add basic Terraform infrastructure

---
+++ infrastructure/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {
+  type    = string
+  default = "westus3"
+}
+
+variable "labelPrefix" {
+  type    = string
+  default = "he000145"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-a09-rg"
+  location = var.location
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.labelPrefix}a09${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
